88f1cf85e4828a2d9c3db84db19dd3fac9ebe572
Ilies Daniel <iliesh.daniel@gmail.com>
2021-12-30 16:20:20 +0000

    better header update

--- header.php
+++ header.php
@@ -1,30 +1,40 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
     <title> BestProperty </title>
     <link rel="stylesheet" href="styles/style.css">
     <link rel="stylesheet" href="./styles/bootstrap.css">
+    <script src="./js/jquery-3.6.0.js"></script>
+    <script src="./js/bootstrap.bundle.js"></script>
+    <script src="https://kit.fontawesome.com/c869041ae8.js" crossorigin="anonymous"></script>
 </head>
-<body>
-  <header id="header">
-      <div class="container">
-          <div class="header-left">
-              <a href="index.php" title="Agentie Imobiliara" class="logo">
-                  <img src="images/bl1.png" />
-              </a>
-          </div>
 
-          <div class="header-right">
-              <ul class="header-menu">
-                  <li><a href="index.php">Acasa</a></li>
-                  <li><a href="proprietati.php">Imobile</a></li>
-                  <li><a href="index.php#aboutus">Despre noi</a></li>
-                  <li><a href="index.php#contactus">Contacte</a></li>
-              </ul>
-              <button id="btn-menu" type="button">menu</button>
-          </div>
-      </div>
-  </header>
+<body>
+    <header id="header">
+        <div class="container">
+        <button id="btn-menu" type="button"><i class="fas fa-bars" ></i></button>
+            <div class="header-left">
+                <a href="index.php" title="Agentie Imobiliara" class="logo">
+                    <img src="images/bl1.png" />
+                </a>
+            </div>
+            <div class="header-right">
+                <ul class="header-menu">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="proprietati.php">Properties</a></li>
+                    <li><a href="index.php#aboutus">About us</a></li>
+                    <li><a href="index.php#contactus">Contact</a></li>
+                </ul>
+                </div>
+
+            <div class="header-element text-center">
+                <a class='btn btn-outline-primary m-1' href="login-user.php">Login</a>
+                <a class='btn btn-outline-primary' href="register-user.php">Register</a>
+            </div>
+
+        </>
+    </header>
